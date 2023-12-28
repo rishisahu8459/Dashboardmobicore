7f17d8537e98b9092e396d8c026e1e948669d461
rishisahu8459 <49360938+rishisahu8459@users.noreply.github.com>
2023-12-28 20:44:57 +0530

    weater shit removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 // src/App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@mui/material';
 
 import './App.css';
 import Navbar from './Components/Navbar';
--- src/Components/HomeContainer.js
+++ src/Components/HomeContainer.js
@@ -2,12 +2,17 @@
 import React from 'react';
 import "../App.css"
 
+
 const HomeContainer = () => {
   return (
     <div className='main-content'>
-      {/* Your main content for the HomeContainer */}
-      <h1>Main Content</h1>
-      <p>Welcome to the Home Container!</p>
+      <div className='left-section'>
+     
+      </div>
+      <div className='right-section'>
+        {/* Content for the right section (40%) */}
+        <h2>Right Section (40%)</h2>
+      </div>
     </div>
   );
 };
